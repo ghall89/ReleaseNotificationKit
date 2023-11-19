1e55467adfb96fce41406f37ce38bb9fd297d053
Graham Hall <graham@ghall.dev>
2023-11-19 16:07:34 -0500

    updater view tweaks

--- Sources/ReleaseNotificationKit/Views/UpdaterView.swift
+++ Sources/ReleaseNotificationKit/Views/UpdaterView.swift
@@ -11,19 +11,15 @@ public struct UpdaterView: View {
 	@AppStorage("lastCheckedVersion") var lastCheckedVersion: String = "v0.0.0"
 	
 	var bundleName: String
-	var currentVersion: SemanticVersion
-	var newVersion: SemanticVersion
-	var downloadURL: URL
-	var releaseNotes: String
-	
+	var newVersion: GitHubRelease
 	
 	public var body: some View {
 		VStack {
 			Text("A new version of \(bundleName) is available!")
 				.font(.headline)
-			Text("\(newVersion.string) (Current Version: \(currentVersion.string))")
+			Text("\(newVersion.name)")
 			ScrollView {
-				Text(releaseNotes)
+				Text(newVersion.body)
 					.padding(6)
 			}
 			.background(Color(NSColor.textBackgroundColor))
@@ -45,9 +41,6 @@ public struct UpdaterView: View {
 #Preview {
 	UpdaterView(
 		bundleName: "My Cool App",
-		currentVersion: SemanticVersion("1.0.1"),
-		newVersion: SemanticVersion("v1.2.1"),
-		downloadURL: URL(string: "https://github.com/ghall89/ReleaseNotificationKit")!,
-		releaseNotes: "Exercitation eu excepteur et minim sed cupidatat tempor laboris reprehenderit ullamco. Amet cillum nostrud cupidatat anim qui dolore dolor nisi sed nostrud dolore duis. Aliqua commodo sunt irure adipiscing est velit consequat est adipiscing fugiat est in lorem sit. Deserunt eu sunt laborum nostrud eiusmod sint nostrud excepteur mollit velit dolore consequat. Nulla cillum eiusmod ipsum ipsum magna et ad consectetur pariatur non deserunt incididunt excepteur amet et."
+		newVersion: GitHubRelease(name: "v1.2.3", html_url: "https://github.com/ghall89/ReleaseNotificationKit", zipball_url: "https://github.com/ghall89/ReleaseNotificationKit", body: "Exercitation eu excepteur et minim sed cupidatat tempor laboris reprehenderit ullamco. Amet cillum nostrud cupidatat anim qui dolore dolor nisi sed nostrud dolore duis. Aliqua commodo sunt irure adipiscing est velit consequat est adipiscing fugiat est in lorem sit. Deserunt eu sunt laborum nostrud eiusmod sint nostrud excepteur mollit velit dolore consequat. Nulla cillum eiusmod ipsum ipsum magna et ad consectetur pariatur non deserunt incididunt excepteur amet et.")
 	)
 }
